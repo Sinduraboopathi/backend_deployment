701d9adb2de6e2b6ce1fcec4ce1e275c3067e4bf
Sinduraboopathi <sinduraboopathi@gmail.com>
2025-03-13 18:12:05 +0530

    Update emailTransporter.js

--- backend/utils/emailTransporter.js
+++ backend/utils/emailTransporter.js
@@ -3,12 +3,24 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// const transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: false, 
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
   },
+  tls: {
+      rejectUnauthorized: false 
+  },
+
 });
 
 export default transporter;
