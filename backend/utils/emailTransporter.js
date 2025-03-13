faa7d11f7e4e7aa8906aa89e49ee06ceb41d0761
Sinduraboopathi <sinduraboopathi@gmail.com>
2025-03-13 18:24:39 +0530

    Update emailTransporter.js

--- backend/utils/emailTransporter.js
+++ backend/utils/emailTransporter.js
@@ -9,18 +9,6 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-// const transporter = nodemailer.createTransport({
-//   host: process.env.EMAIL_HOST,
-//   port: process.env.EMAIL_PORT,
-//   secure: false, 
-//   auth: {
-//       user: process.env.EMAIL_USER,
-//       pass: process.env.EMAIL_PASS,
-//   },
-//   tls: {
-//       rejectUnauthorized: false 
-//   },
-
-// });
+});
 
 export default transporter;
