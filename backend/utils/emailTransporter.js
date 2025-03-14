ddee628022c155a14ee4aea100b35a85254b3e2c
Sinduraboopathi <sinduraboopathi@gmail.com>
2025-03-14 10:14:34 +0530

    Update emailTransporter.js

--- backend/utils/emailTransporter.js
+++ backend/utils/emailTransporter.js
@@ -1,14 +1,45 @@
-import nodemailer from 'nodemailer';
-import dotenv from 'dotenv';
+// import nodemailer from 'nodemailer';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
+// export default transporter;
+
+
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false, 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false, 
+  },
+  logger: true, 
+  debug: true, 
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("SMTP Error:", error);
+  } else {
+    console.log("SMTP Server is ready");
+  }
 });
 
 export default transporter;
